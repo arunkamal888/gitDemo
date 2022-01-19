a4ae0a6e3e6ba795db4039ae883b9060c2e43686
arunkamal888 <arunkamal888@gmail.com>
2022-01-19 12:13:35 +0530

    new project to git

---
+++ src/orgstudent/Student.java
@@ -0,0 +1,37 @@
+package orgstudent;
+
+import java.util.Scanner;
+
+public class Student 
+{
+	
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		byte a= sc.nextByte();
+		System.out.println("student ID:"+a);
+		String s=sc.next();
+		String k=sc.next();
+		System.out.println("Student name:"+s+k);
+		String h=sc.next();
+		System.out.println("Student Email:"+h);
+		long n=sc.nextLong();
+		System.out.println("Student phone number:"+n);
+		String g=sc.next();
+		System.out.println("Student sec:"+g);
+		String i=sc.next();
+		System.out.println("Student sec:"+i);
+		String r=sc.next();
+		System.out.println("Student name:"+r);
+		
+		
+		
+		
+		
+		
+		
+	
+	
+
+}
+}
